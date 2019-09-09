2c377074b252ce97f75c734b5b9cd1a92972c090
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-09-09 10:40:44 +0200

    New computejob resource

--- operator/operator_main.py
+++ operator/operator_main.py
@@ -3,6 +3,7 @@
 import asyncio
 import threading
 import logging
+import time
 
 import kopf
 from k8s_utils import *
@@ -21,7 +22,7 @@ async def create_workflow_handler(**kwargs):
 
 
 @kopf.on.create('oceanprotocol.com', 'v1alpha', 'workflows')
-async def create_workflow(**kwargs):
+def create_workflow(**kwargs):
     body = kwargs['body']
     attributes = body['spec']['metadata']['service'][0]['attributes']
 
@@ -48,20 +49,20 @@ async def create_workflow(**kwargs):
     # Wait configure pod to finish
     while not wait_finish_job(body['metadata']['namespace'], f"{body['metadata']['name']}-configure-job"):
         logger.info("Waiting configure pod to finish")
-        await asyncio.sleep(10.0)
+        time.sleep(10.0)
 
     # Algorithm job
     create_algorithm_job(body, logger)
     # Wait configure pod to finish
     while not wait_finish_job(body['metadata']['namespace'], f"{body['metadata']['name']}-algorithm-job"):
         logger.info("Waiting algorithm pod to finish")
-        await asyncio.sleep(10.0)
+        time.sleep(10.0)
 
     # Publish job
     create_publish_job(body, logger)
     while not wait_finish_job(body['metadata']['namespace'], f"{body['metadata']['name']}-publish-job"):
         logger.info("Waiting publish pod to finish")
-        await asyncio.sleep(10.0)
+        time.sleep(10.0)
 
     return {'message': "Creating workflow finished"}
 
@@ -76,3 +77,9 @@ def update_workflow(body, spec, old, new, diff, logger, **_):
 def delete_workflow(body, logger, **_):
     logger.warning(f"Deleted {body['metadata']['name']}")
     return {'message': "Deleted workflow finished"}
+
+
+@kopf.on.delete('oceanprotocol.com', 'v1alpha', 'computejob')
+def create_compute_job(body, logger, **_):
+    logging.info(f'Creating computejob for {body["spec"]["type"]} of workflow {body["spec"]["workflow"]}')
+    create
--- operator/resources.py
+++ operator/resources.py
@@ -130,7 +130,7 @@ def create_algorithm_job(body, logger):
     job['spec']['template']['spec']['containers'][0]['command'] = ['sh', '-c', OperatorConfig.POD_ALGORITHM_INIT_SCRIPT]
     job['spec']['template']['spec']['containers'][0]['image'] = \
         f"{attributes['workflow']['stages'][0]['requirements']['container']['image']}" \
-        f":{attributes['workflow']['stages'][0]['requirements']['container']['tag']}"
+            f":{attributes['workflow']['stages'][0]['requirements']['container']['tag']}"
 
     # Env
     did_input_0 = [e for e in attributes['workflow']['stages'][0]['input'] if e['index'] == 0][0]
@@ -241,4 +241,114 @@ def create_publish_job(body, logger):
 
     batch_client = kubernetes.client.BatchV1Api()
     obj = batch_client.create_namespaced_job(body['metadata']['namespace'], job)
+    logger.info(f"{obj.kind} {obj.metadata.name} created")
+
+
+def create_job_from_computejob(computejob_body, logger):
+    namespace = computejob_body['metadata']['namespace']
+    jobtype = computejob_body['spec']['type']
+    # We need to get the workflow body
+    custom_object_client = kubernetes.client.CustomObjectsApi()
+    workflow_body = custom_object_client.get_namespaced_custom_object(group='oceanprotocol.com', version='v1alpha',
+                                                                      namespace=namespace, plural='computejobs',
+                                                                      name=computejob_body['spec']['workflow'])
+    with open("templates/job-template.yaml", 'r') as stream:
+        try:
+            job = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    job['metadata']['labels']['app'] = workflow_body['metadata']['name']
+    job['metadata']['labels']['workflow'] = workflow_body['metadata']['labels']['workflow']
+    job['metadata']['labels']['component'] = 'configure'
+    job['metadata']['name'] = f"{workflow_body['metadata']['name']}-{jobtype}-job"
+    job['metadata']['namespace'] = workflow_body['metadata']['namespace']
+
+    job['spec']['template']['metadata']['labels']['workflow'] = workflow_body['metadata']['labels']['workflow']
+    job['spec']['template']['metadata']['labels']['component'] = 'configure'
+
+    # Volumes
+    job['spec']['template']['spec']['volumes'] = []
+
+    # Data volume
+    job['spec']['template']['spec']['volumes'].append(
+        {'name': 'download', 'persistentVolumeClaim': {'claimName': workflow_body['metadata']['name']}})
+    volume_mount = {'mountPath': '/data', 'name': 'download', 'readOnly': False}
+    job['spec']['template']['spec']['containers'][0]['volumeMounts'] = []
+    job['spec']['template']['spec']['containers'][0]['volumeMounts'].append(volume_mount)
+
+    # Workflow config volume
+    job['spec']['template']['spec']['volumes'].append(
+        {'name': 'workflow', 'configMap': {'defaultMode': 420, 'name': workflow_body['metadata']['name']}})
+    volume_mount = {'mountPath': '/workflow.yaml', 'name': 'workflow', 'subPath': 'workflow.yaml'}
+    job['spec']['template']['spec']['containers'][0]['volumeMounts'].append(volume_mount)
+    volume_mount = {'mountPath': '/workflow.json', 'name': 'workflow', 'subPath': 'workflow.json'}
+    job['spec']['template']['spec']['containers'][0]['volumeMounts'].append(volume_mount)
+
+    job['spec']['template']['metadata']['labels']['component'] = jobtype
+
+    envs = []
+
+    if jobtype == 'configure':
+        init_script = OperatorConfig.POD_CONFIGURATION_INIT_SCRIPT
+        image = OperatorConfig.POD_CONFIGURATION_CONTAINER
+
+        envs.append({'name': 'CREDENTIALS', 'value': OperatorConfig.ACCOUNT_JSON})
+        envs.append({'name': 'PASSWORD', 'value': OperatorConfig.ACCOUNT_PASSWORD})
+        envs.append({'name': 'INPUTS', 'value': OperatorConfig.INPUTS_FOLDER})
+        envs.append({'name': 'TRANSFORMATIONS', 'value': OperatorConfig.TRANSFORMATIONS_FOLDER})
+        envs.append({'name': 'VOLUME', 'value': '/data'})
+        envs.append({'name': 'NODE', 'value': ExternalURLs.KEEPER_URL})
+        envs.append({'name': 'WORKFLOW', 'value': OperatorConfig.WORKFLOW})
+
+    elif jobtype == 'algorithm':
+        init_script = OperatorConfig.POD_ALGORITHM_INIT_SCRIPT
+        attributes = workflow_body['spec']['metadata']['service'][0]['attributes']
+        image = \
+            f"{attributes['workflow']['stages'][0]['requirements']['container']['image']}" \
+            f":{attributes['workflow']['stages'][0]['requirements']['container']['tag']}"
+
+        # Env
+        did_input_0 = [e for e in attributes['workflow']['stages'][0]['input'] if e['index'] == 0][0]
+        did_input_1 = [e for e in attributes['workflow']['stages'][0]['input'] if e['index'] == 1][0]
+        did_transformation = attributes['workflow']['stages'][0]['transformation']
+
+        env_did0 = f"datafile.{did_input_0['id'].replace('did:op:', '')}.0"
+        env_did1 = f"datafile.{did_input_1['id'].replace('did:op:', '')}.0"
+        env_transformation = f"datafile.{did_transformation['id'].replace('did:op:', '')}.0"
+        envs.append({'name': 'VOLUME', 'value': '/data'})
+        envs.append({'name': 'DID_INPUT1', 'value': env_did0})
+        envs.append({'name': 'DID_INPUT2', 'value': env_did1})
+        envs.append({'name': 'TRANSFORMATION_DID', 'value': env_transformation})
+
+    elif jobtype == 'publish':
+        init_script = OperatorConfig.POD_PUBLISH_INIT_SCRIPT
+        image = OperatorConfig.POD_PUBLISH_CONTAINER
+
+        envs.append({'name': 'CREDENTIALS', 'value': OperatorConfig.ACCOUNT_JSON})
+        envs.append({'name': 'PASSWORD', 'value': OperatorConfig.ACCOUNT_PASSWORD})
+        envs.append({'name': 'INPUTS', 'value': OperatorConfig.INPUTS_FOLDER})
+        envs.append({'name': 'TRANSFORMATIONS', 'value': OperatorConfig.TRANSFORMATIONS_FOLDER})
+        envs.append({'name': 'VOLUME', 'value': '/data'})
+        envs.append({'name': 'NODE', 'value': ExternalURLs.KEEPER_URL})
+        envs.append({'name': 'WORKFLOW', 'value': OperatorConfig.WORKFLOW})
+        envs.append({'name': 'AWS_ACCESS_KEY_ID', 'value': OperatorConfig.AWS_ACCESS_KEY_ID})
+        envs.append({'name': 'AWS_SECRET_ACCESS_KEY', 'value': OperatorConfig.AWS_SECRET_ACCESS_KEY})
+        envs.append({'name': 'AQUARIUS_URL', 'value': ExternalURLs.AQUARIUS_URL})
+        envs.append({'name': 'BRIZO_URL', 'value': ExternalURLs.BRIZO_URL})
+        envs.append({'name': 'SECRET_STORE_URL', 'value': ExternalURLs.SECRET_STORE_URL})
+
+    else:
+        logger.error(f'ComputeJob type {jobtype} not recognized')
+        return -1
+
+    job['spec']['template']['spec']['containers'][0]['env'] = envs
+    job['spec']['template']['spec']['containers'][0]['image'] = image
+    job['spec']['template']['spec']['containers'][0]['command'] = ['sh', '-c', init_script]
+
+    # Run job
+    kopf.adopt(job, owner=computejob_body)
+
+    batch_client = kubernetes.client.BatchV1Api()
+    obj = batch_client.create_namespaced_job(namespace, job)
     logger.info(f"{obj.kind} {obj.metadata.name} created")

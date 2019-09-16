928316a3111977a82b89ce24f8ecf6978165885c
Aitor <aaitor@gmail.com>
2019-09-16 15:47:54 +0200

    coming back to previous version

--- operator_engine/operator_main.py
+++ operator_engine/operator_main.py
@@ -9,6 +9,8 @@ import kopf
 from k8s_utils import *
 from resources import *
 
+from operator_engine.resources import create_configmap_workflow
+
 logger = logging.getLogger('ocean-operator')
 logger.setLevel(logging.DEBUG)
 

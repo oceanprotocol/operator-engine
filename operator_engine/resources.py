f454974bb42b621f12fe7eae5d4fb45cf4d43971
Alex Coseru <alex.coseru@gmail.com>
2022-07-04 18:57:47 +0300

    Update resources.py

--- operator_engine/resources.py
+++ operator_engine/resources.py
@@ -275,8 +275,8 @@ def create_algorithm_job(body, logger, resources):
         "stages"
     ][0]["algorithm"]["container"]["checksum"].startswith("sha256:"):
         job["spec"]["template"]["spec"]["containers"][0]["image"] = (
-            f"{metadata['stages'][0]['algorithm']['container']['image']}@"
-            f":{metadata['stages'][0]['algorithm']['container']['checksum']}"
+            f"{metadata['stages'][0]['algorithm']['container']['image']}"
+            f"@{metadata['stages'][0]['algorithm']['container']['checksum']}"
         )
     else:
         job["spec"]["template"]["spec"]["containers"][0]["image"] = (
